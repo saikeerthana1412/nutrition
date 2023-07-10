e91f124d98fde153bb59eb5e3dc03b72180accf9
saikeerthana1412 <137150706+saikeerthana1412@users.noreply.github.com>
2023-07-10 20:16:09 +0530

    Update urls.py

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns= [
     path('', views.say_hello)
+    path('submitrequest/', views.submit_request),
 ]
 
 #urlpatterns+=staticfiles_urlpatterns()
